0c4ec85e7b9906da48a381b1169dec05815ac8df
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-02 15:17:09 +0400

    add config and another files

--- schedule-app/src/app/app.routes.ts
+++ schedule-app/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-
+import { Register
+  
+ } from './pages/register/register';
 export const routes: Routes = [
      { 
     path: '', 
@@ -11,6 +13,11 @@ export const routes: Routes = [
     path: 'login', 
     redirectTo: '',                 
     pathMatch: 'full'
+  },
+  {
+    path:'register',
+    component:Register,
+    pathMatch:'full'
   },
  
   { path: '**', redirectTo: '' }

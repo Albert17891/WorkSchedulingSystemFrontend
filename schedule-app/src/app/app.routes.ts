8d4f411d32665344fe109b0290bac46e4875fe67
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-02 09:38:52 +0400

    add interceptors and config file

--- schedule-app/src/app/app.routes.ts
+++ schedule-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { 
+    path: '', 
+    component: Login,      
+    pathMatch: 'full'              
+  },
+  { 
+    path: 'login', 
+    redirectTo: '',                 
+    pathMatch: 'full'
+  },
+ 
+  { path: '**', redirectTo: '' }
+];

730b0c3a1aab44c4e414859952b356ec4722c232
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-03 15:26:49 +0300

    add admin daspoard

---
+++ schedule-app/src/app/pages/admin-dashboard/admin-dashboard.ts
@@ -0,0 +1,60 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Schedule } from '../../services/schedule'; 
+import { ScheduleDetailsDto } from '../../models/schedule-models';
+import { ScheduleStatus } from '../../models/ScheduleStatus.enum';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  imports: [CommonModule],
+  templateUrl: './admin-dashboard.html',
+  styleUrl: './admin-dashboard.scss',
+})
+export class AdminDashboard {
+   private scheduleService = inject(Schedule);
+  
+   pendingSchedules: ScheduleDetailsDto[] = [];
+   isLoading = false;  
+
+   ngOnInit() {
+    this.loadPending();
+  }
+
+  loadPending() {
+    this.isLoading = true;
+    this.scheduleService.getPendingSchedules().subscribe({
+      next: (data) => {
+        this.pendingSchedules = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Failed to load schedules', err);
+        this.isLoading = false;
+      }
+    });
+  }
+
+  approve(id: string) {
+    this.processDecision(id, ScheduleStatus.Approved);
+  }
+  
+  reject(id: string) {
+    this.processDecision(id, ScheduleStatus.Rejected);
+  }
+
+  private processDecision(id: string, status: ScheduleStatus) {
+    if(!confirm(`Are you sure you want to ${status === 1 ? 'Approve' : 'Reject'} this request?`)) return;
+
+    this.scheduleService.changeStatus(id, status).subscribe({
+      next: () => {
+       
+        this.pendingSchedules = this.pendingSchedules.filter(s => s.id !== id);
+        
+      },
+      error: (err) => {
+        alert('Error updating status');
+        console.error(err);
+      }
+    });
+  }
+}
